fe28164e97899f294b7f9bd2c2409425cc445116
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-10-06 22:02:48 +0500

    Refactor LoginButton to use custom useLoginMutation hook and add ESLint configuration

--- shared/components/LoginButton.tsx
+++ shared/components/LoginButton.tsx
@@ -1,36 +1,9 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Button from "./ui/button";
-import { client } from "entities/api/client";
+import useLoginMutation from "shared/hooks/useLoginMutation";
 
 export default function LoginButton() {
-	const queryClient = useQueryClient();
 	const callbackUrl = "http://localhost:5173/oauth";
-	const mutation = useMutation({
-		mutationFn: async ({ code }: { code: string }) => {
-			const response = await client.POST("/auth/login", {
-				body: {
-					code,
-					redirectUri: callbackUrl,
-					rememberMe: true,
-					accessTokenTTL: "1d",
-				},
-			});
-			if (response.error) {
-				throw new Error(response.error.message);
-			}
-			return response.data;
-		},
-		onSuccess: (data) => {
-			if (!data) {
-				throw new Error("Invalid Response");
-			}
-			localStorage.setItem("musicfunapi-accessToken", data.accessToken);
-			localStorage.setItem("musicfunapi-refreshToken", data.refreshToken);
-			queryClient.invalidateQueries({
-				queryKey: ["auth", "me"],
-			});
-		},
-	});
+	const mutation = useLoginMutation(callbackUrl);
 	const handleLogin = () => {
 		window.addEventListener("message", handleOauthMessage);
 		window.open(`${import.meta.env.VITE_BASE_URL}/auth/oauth-redirect?callbackUrl=${callbackUrl}`, "apihub-oauth2", "width=500,height=600");
--- shared/hooks/useLoginMutation.ts
+++ shared/hooks/useLoginMutation.ts
@@ -1,5 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useMeQuery } from "./useMeQuery";
+import { client } from "entities/api/client";
 
-export default function useLoginMutation() {
+export default function useLoginMutation(callbackUrl: string) {
 	const { data } = useMeQuery();
+	const queryClient = useQueryClient();
+	const mutation = useMutation({
+		mutationFn: async ({ code }: { code: string }) => {
+			const response = await client.POST("/auth/login", {
+				body: {
+					code,
+					redirectUri: callbackUrl,
+					rememberMe: true,
+					accessTokenTTL: "1d",
+				},
+			});
+			if (response.error) {
+				throw new Error(response.error.message);
+			}
+			return response.data;
+		},
+		onSuccess: (data) => {
+			if (!data) {
+				throw new Error("Invalid Response");
+			}
+			localStorage.setItem("musicfunapi-accessToken", data.accessToken);
+			localStorage.setItem("musicfunapi-refreshToken", data.refreshToken);
+			queryClient.invalidateQueries({
+				queryKey: ["auth", "me"],
+			});
+		},
+	});
+	return mutation;
 }
